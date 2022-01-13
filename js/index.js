f0f3544ad588233bd5f3478bd1ddd13f45728d55
Natacha Vergara <ntchvergara@gmail.com>
2022-01-13 18:23:33 -0300

    tratando de mejorar el header

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@ const cardsCachorros = document.querySelector('.cachorros')
 
 const carousel = document.querySelector('.root')
 
+const mp4 = document.querySelector('.videos')
+
 fetch('slider.json')
     .then(promesa => promesa.json())
     .then(data => {
@@ -40,5 +42,20 @@ fetch('perritos.json')
             }
         )
     })
+
+
+
+fetch('videos.json')
+    .then(promesa => promesa.json())
+    .then(data => data.map(video => {
+        mp4.innerHTML += `
+        <div class="col-12 col-lg-3">
+        <video width="320" height="240" autoplay muted loop>
+            <source src=${video.vid} type="video/mp4">
+            Your browser does not support the video tag.
+        </video>
+        </div>
+        `
+    }))
 
 
